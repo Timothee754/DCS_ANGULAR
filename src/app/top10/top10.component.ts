53cb2c54356b15870bd4c29e6c2f631c011be4a5
Teemo_v4 <147592228+Timothee754@users.noreply.github.com>
2025-03-16 22:28:43 +0100

    Tableau top10 fonctionnel

--- src/app/top10/top10.component.ts
+++ src/app/top10/top10.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Text } from '../text';
-
+import { ListeApp } from '../liste-app';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-top10',
   imports: [],
   templateUrl: './top10.component.html',
   styleUrl: './top10.component.css'
 })
-export class Top10Component {
-  constructor() {}
-  variabletest:Text={value:""};
-  ngOnInit() {
-    this.variabletest.value = "Hello World!";
+export class Top10Component implements OnInit {
+  private url = "http://127.0.0.1:8000/api/centresactivite/top10byAp";
+  public liste_app: ListeApp[] = [];
+  private subscription!: Subscription;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.subscription = this.http.get<ListeApp[]>(this.url).subscribe(app_api => {
+      this.liste_app = app_api;
+    });
   }
-  }
+}
