8ab2a2b52ee83fb7b53f163d85a4e5798e87e92c
Teemo_v4 <147592228+Timothee754@users.noreply.github.com>
2025-03-16 18:42:20 +0100

    Instanciation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Top10Component } from './top10/top10.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'top10', component: Top10Component}
+];
---
+++ src/app/top10/top10.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top10',
+  imports: [],
+  templateUrl: './top10.component.html',
+  styleUrl: './top10.component.css'
+})
+export class Top10Component {
+
+}
