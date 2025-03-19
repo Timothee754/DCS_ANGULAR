ede033ea3bc6c359b074ed4168e3a81652177ad7
Teemo_v4 <147592228+Timothee754@users.noreply.github.com>
2025-03-19 23:09:03 +0100

    Fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { Top10Component } from './top10/top10.component';
 import {CinqplusgrandpardateComponent} from './cinqplusgrandpardate/cinqplusgrandpardate.component'
-import {TestComponent} from './test/test.component'
 
 
 export const routes: Routes = [
     {path:'top10', component: Top10Component},
   {path:'cinq', component: CinqplusgrandpardateComponent},
-  {path: 'test', component: TestComponent}
 ];
--- src/app/cinqplusgrandpardate/cinqplusgrandpardate.component.ts
+++ src/app/cinqplusgrandpardate/cinqplusgrandpardate.component.ts
@@ -1,8 +1,9 @@
 import Chart from 'chart.js/auto';
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {Subscription} from 'rxjs';
-import {ListeApp} from '../liste-app';
+import {ClientData} from '../clientdata';
 import {HttpClient} from '@angular/common/http';
+import {ClientdataService} from '../clientdata.service';
 
 @Component({
   selector: 'app-cinqplusgrandpardate',
@@ -12,54 +13,14 @@ import {HttpClient} from '@angular/common/http';
 })
 export class CinqplusgrandpardateComponent {
   private url = "http://127.0.0.1:8000/api/centresactivite/cinqplusgrandpardate";
-  public liste_app: ListeApp[] = [];
-  private subscription!: Subscription;
-
-  constructor(private http: HttpClient) {}
+  private clientdataservice=inject(ClientdataService);
+  liste_clients_data = this.clientdataservice.clients
 
   ngOnInit(): void {
-    this.subscription = this.http.get<ListeApp[]>(this.url).subscribe(app_api => {
-      this.liste_app = app_api;
-    });
+    this.clientdataservice.getcinqgrandsclients().subscribe()
   }
 
-  createChart(): void {
-      const ctx = document.getElementById('cinqplusgrand') as HTMLCanvasElement;
-      this.chart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-          labels: this.liste_app.map(app => app.NomClient),
-          datasets: [{
-            label: 'Total Prix',
-            data: this.liste_app.map(app => app.total_prix),
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
-  }
+
 
 
 }
